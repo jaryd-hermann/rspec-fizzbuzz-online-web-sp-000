d76b4e32336cb9b4e6ae795f90f58a2c874f8ae1
Jaryd Hermann <hermannjaryd@gmail.com>
2021-01-27 00:08:22 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,10 +3,11 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
+    fizz_3 = int
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
-  elsif int % 3 == 0 && int % 5  == 0
+  elsif int % 3 == 0 && (int % 5 ) == 0
     puts "FizzBuzz"
   else
     puts "#{number}"
