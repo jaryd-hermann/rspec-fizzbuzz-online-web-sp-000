ed5fe790af5af5870f666195d5c24cc53d051b87
Jaryd Hermann <hermannjaryd@gmail.com>
2021-01-27 00:17:13 +0000

    Automatically backed up by Learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(n)
   if (n % 3 == 0)
-    puts "Fizz"
+    "Fizz"
   elsif (n % 5 == 0)
-    puts <<  "Buzz"
+   "Buzz"
   elsif ((n % 3 == 0) && (n % 5 == 0))
-    puts <<  "FizzBuzz"
+  "FizzBuzz"
   else
     puts <<  n
 end
